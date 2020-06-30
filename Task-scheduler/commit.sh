ed74a4736e90f4db11e25217837574066eb8ef46
Kyle Tolliver <43971202+kctolli@users.noreply.github.com>
2020-06-30 15:17:36 -0700

    bash-commit : Update at 15:17 on 06/30/2020

--- Task-scheduler/commit.sh
+++ Task-scheduler/commit.sh
@@ -4,5 +4,6 @@ git add --all
 timestamp() {
   date +"at %H:%M on %m/%d/%Y"
 }
-git commit -am "auto-commit : Update $(timestamp)"
+git commit -am "bash-commit : Update $(timestamp)"
+git pull <remote>
 git push origin master
